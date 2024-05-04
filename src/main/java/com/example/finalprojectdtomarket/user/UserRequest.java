00bedc71a84f83397fd9a9da1bd6fae14f3182a4
hyeonnit <lhyeonnit@gmail.com>
2024-05-04 17:06:51 +0900

    UserRequest joinDTO

--- src/main/java/com/example/finalprojectdtomarket/user/UserRequest.java
+++ src/main/java/com/example/finalprojectdtomarket/user/UserRequest.java
@@ -1,4 +1,29 @@
 package com.example.finalprojectdtomarket.user;
 
+import jakarta.persistence.Column;
+import lombok.Data;
+
+import java.sql.Date;
+
 public class UserRequest {
+
+    @Data
+    public static class JoinDTO {
+        private String password;
+        private String username;
+        private String phone;
+        private String email;
+        private Date birth;
+        private Integer role;
+        public User toEntity(){
+            return User.builder()
+                    .username(username)
+                    .password(password)
+                    .phone(phone)
+                    .birth(birth)
+                    .email(email)
+                    .role(role)
+                    .build();
+        }
+    }
 }
