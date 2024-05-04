2f00322f0b0dc05bbb77b1896fa2419d334c7d05
hyeonnit <lhyeonnit@gmail.com>
2024-05-04 17:35:29 +0900

    UserService getUsername

--- src/main/java/com/example/finalprojectdtomarket/user/UserJPARepository.java
+++ src/main/java/com/example/finalprojectdtomarket/user/UserJPARepository.java
@@ -1,6 +1,10 @@
 package com.example.finalprojectdtomarket.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 public interface UserJPARepository extends JpaRepository<User, Integer> {
+    User findByUsername(@PathVariable("username") String username);
 }
