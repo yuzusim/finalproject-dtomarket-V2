9ddc82f34f9876091853c1c93cf1731bfd70197b
hyeonnit <lhyeonnit@gmail.com>
2024-05-04 17:11:17 +0900

    UserResponse JoinDTO

--- src/main/java/com/example/finalprojectdtomarket/user/UserResponse.java
+++ src/main/java/com/example/finalprojectdtomarket/user/UserResponse.java
@@ -1,4 +1,26 @@
 package com.example.finalprojectdtomarket.user;
 
+import lombok.Data;
+
+import java.sql.Date;
+
 public class UserResponse {
+    @Data
+    public static class JoinDTO {
+        private String password;
+        private String username;
+        private String phone;
+        private String email;
+        private Date birth;
+        private Integer role;
+
+        public JoinDTO(User user) {
+            this.password = user.getPassword();
+            this.username = user.getUsername();
+            this.phone = user.getPhone();
+            this.email = user.getEmail();
+            this.birth = user.getBirth();
+            this.role = user.getRole();
+        }
+    }
 }
