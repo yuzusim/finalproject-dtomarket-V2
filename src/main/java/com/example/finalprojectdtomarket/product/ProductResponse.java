284e8db673b2cae027c376290fd7f4857f31ef5c
yuzusim <yuzusim@gmail.com>
2024-05-06 20:05:54 +0900

    product/list 목록보기, 이미지, 더미 수정 완료

--- src/main/java/com/example/finalprojectdtomarket/product/ProductResponse.java
+++ src/main/java/com/example/finalprojectdtomarket/product/ProductResponse.java
@@ -1,4 +1,24 @@
 package com.example.finalprojectdtomarket.product;
 
+import lombok.Data;
+
 public class ProductResponse {
+
+    //상품 목록보기
+    @Data
+    public static class ListDTO{
+        private Integer id;
+        private String name;
+        private Integer price;
+        private Integer qty;
+        private String img;
+
+        public ListDTO(Product product) {
+            this.id = product.getId();
+            this.name = product.getName();
+            this.price = product.getPrice();
+            this.qty = product.getQty();
+            this.img = product.getImg();
+        }
+    }
 }
--- src/main/java/com/example/finalprojectdtomarket/product/ProductService.java
+++ src/main/java/com/example/finalprojectdtomarket/product/ProductService.java
@@ -1,11 +1,20 @@
 package com.example.finalprojectdtomarket.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     private final ProductJPARepository productJPARepository;
+
+    //상품 목록보기
+    public List<Product> findAll() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        return productJPARepository.findAll(sort); // return에 sort 객체 안 넣어주면 DESC 안 됨
+    }
 
 }
